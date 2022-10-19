861589f95e7fba8decaca34cb68954d8e4e7bdc1
Kamalesh <pkamalesh2007@gmail.com>
2022-10-19 11:51:39 +0530

    Commit Changes in  MVCCoreLab1

---
+++ KT/MVCCoreLab1/MVCCoreLab1/Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCCoreLab1.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Add()
+        {
+            return View("CustomerAdd");
+        }
+
+        public IActionResult Rec(string Value1)
+        {
+            return Content(Value1);
+        }
+
+        public IActionResult Update()
+        {
+            return View();
+        }
+    }
+}
