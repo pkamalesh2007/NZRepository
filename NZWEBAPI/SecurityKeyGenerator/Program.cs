5aec1224b27d425cb9a8978b82929988f8481d5b
Kamalesh <pkamalesh2007@gmail.com>
2023-02-11 18:40:39 +0530

    Commit for after updating the SymmetricSecurityKey Generator

---
+++ NZWEBAPI/SecurityKeyGenerator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+byte[] key = new byte[32];
+using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+{
+    randomNumberGenerator.GetBytes(key);
+}
+
+// Create a SymmetricSecurityKey from the random key
+SymmetricSecurityKey securityKey = new SymmetricSecurityKey(key);
+
+Console.WriteLine(securityKey.ToString());//EG_mNJVLGjEMf_YkxKUSFRmOxy0ARVHE54YzxDXOJw4
